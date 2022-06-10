403e0ed31594e4c395f3b7e816c67c8881e6bc9f
Nils Krüger <nils.krueger@tum.de>
2022-06-10 23:26:58 +0200

    Initial commit

---
+++ src/main/java/com/github/fhirvaccinationpass/ibmfhirig/DEMedizininformatikinitiativeKerndatensatzPersonResourceProvider.java
@@ -0,0 +1,10 @@
+package com.github.fhirvaccinationpass.ibmfhirig;
+
+import com.ibm.fhir.registry.util.PackageRegistryResourceProvider;
+
+public class DEMedizininformatikinitiativeKerndatensatzPersonResourceProvider extends PackageRegistryResourceProvider {
+    @Override
+    public String getPackageId() {
+        return "de.medizininformatikinitiative.kerndatensatz.person";
+    }
+}
