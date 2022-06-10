1ca6c1eca449469219c29a4e0a94a7eb0bae68f2
tklausen <102419021+tklausen@users.noreply.github.com>
2022-06-11 00:07:27 +0200

    add DEBasisprofilR4 and DEMedizininformatikinitiativeKerndatensatzMeta

---
+++ src/main/java/com/github/fhirvaccinationpass/ibmfhirig/DEBasisprofilR4ResourceProvider.java
@@ -0,0 +1,10 @@
+package com.github.fhirvaccinationpass.ibmfhirig;
+
+import com.ibm.fhir.registry.util.PackageRegistryResourceProvider;
+
+public class DEBasisprofilR4ResourceProvider extends PackageRegistryResourceProvider {
+    @Override
+    public String getPackageId() {
+        return "de.basisprofil.r4";
+    }
+}
---
+++ src/main/java/com/github/fhirvaccinationpass/ibmfhirig/DEMedizininformatikinitiativeKerndatensatzMetaResourceProvider.java
@@ -0,0 +1,10 @@
+package com.github.fhirvaccinationpass.ibmfhirig;
+
+import com.ibm.fhir.registry.util.PackageRegistryResourceProvider;
+
+public class DEMedizininformatikinitiativeKerndatensatzMetaResourceProvider extends PackageRegistryResourceProvider {
+    @Override
+    public String getPackageId() {
+        return "de.medizininformatikinitiative.kerndatensatz.meta";
+    }
+}
